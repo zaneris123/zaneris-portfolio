6731555206c4d22344a7b3536e01b40d92781cbf
zaneris123 <zaneris123@gmail.com>
2023-12-17 20:36:25 +0000

    Create scene + Imported basic models

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import { Environment, OrbitControls } from '@react-three/drei';
+import { Canvas, useLoader } from '@react-three/fiber';
+import React from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Canvas camera={{ fov: 65, position: [5, 5, 5] }}>
+          <OrbitControls/>
+          <MyStage/>
+          <MyDesk/>
+          <MyBoard/>
+          <MyShelf/>
+          <Environment preset='sunset' background />
+        </Canvas>
       </header>
     </div>
   );
 }
+
+function MyStage(){
+  const stageMesh = React.useRef();
+  const stage = useLoader(GLTFLoader, "Stage.gltf")
+  return (
+    <mesh ref={stageMesh}>
+      <primitive object={stage.scene}/>
+    </mesh>
+  )
+}
+
+function MyDesk(){
+  const deskMesh = React.useRef();
+  const desk = useLoader(GLTFLoader, "Desk.gltf")
+  return (
+    <mesh ref={deskMesh}>
+      <primitive object={desk.scene}/>
+    </mesh>
+  )
+}
+
+function MyBoard(){
+  const boardMesh = React.useRef();
+  const board = useLoader(GLTFLoader, "Board.gltf")
+  return (
+    <mesh ref={boardMesh}>
+      <primitive object={board.scene}/>
+    </mesh>
+  )
+}
+
+function MyShelf(){
+  const shelfMesh = React.useRef();
+  const shelf = useLoader(GLTFLoader, "Shelf.gltf")
+
+  return (
+    <mesh ref={shelfMesh}>
+      <primitive object={shelf.scene}/>
+    </mesh>
+  )
+}
+
+
 
 export default App;
